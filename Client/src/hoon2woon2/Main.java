c44a98dec673e97c941867405dff42983112c909
KimJihoon18 <momo990305@gmail.com>
2020-06-10 22:21:55 +0900

    restart

--- Client/src/hoon2woon2/Main.java
+++ Client/src/hoon2woon2/Main.java
@@ -1,23 +1,16 @@
 package hoon2woon2;
 
-import javax.swing.JFrame;
-
 import org.psnbtech.Tetris;
 
-public class Main extends JFrame{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	
-	
+public class Main {
 	public static Tetris tetris;
 	public static Client client;
+	public static MultiPlay multi;
 	
 	public static void main(String[] args) {
 		client = new Client();
 		tetris = new Tetris(client);
 		tetris.startGame();
+		multi = new MultiPlay(client);
 	}
 }
